303b3daee3c398df9f9188e544c6c073e253f6c6
Abzelhan Sahitov <abzelhan.sahitov@robusta.team>
2020-08-15 23:51:41 +0600

    Init project skeleton

---
+++ src/main/java/kz/applefriends/engine/AppleFriendsApplication.java
@@ -0,0 +1,13 @@
+package kz.applefriends.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/applefriends/engine/AppleFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.applefriends.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
